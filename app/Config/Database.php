f5b718e1f8cd3b5f5b0d7b7770f5673dea602827
BIOCOMUNI <sgi.biodiversidad@conafor.gob.mx>
2025-02-16 13:13:42 -0600

    hotfix DB

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -66,7 +66,7 @@ class Database extends Config
         'compress' => false,
         'strictOn' => false,
         'failover' => [],
-        'port'     => 3307,
+        'port'     => 3306,
         'saveQueries' => true,
     ];
 
